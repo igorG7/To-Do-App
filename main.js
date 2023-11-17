13027635e896a93185b4364e08838cc06e37d77b
igorG7 <giovane167@outlook.com>
2023-11-17 13:04:39 -0300

    Capturando conteúdo do input

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
 import { themeChange } from "./util/theme.js";
 
 const theme_button = document.querySelector("#theme-button");
+const input_create = document.querySelector("#input-create");
 
 theme_button.addEventListener("click", themeChange);
+
+let contentTask;
+
+input_create.addEventListener("input", (e) => {
+  contentTask = e.target.value;
+
+  console.log(contentTask);
+});
+
+input_create.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    allObj.push({ taskText: `${contentTask}`, status: "ativo" });
+    console.log(allObj);
+    input_create.value = "";
+  }
+});
+
+let allObj = [
+  {
+    taskText: "Fazer almoço",
+    status: "ativo",
+  },
+
+  {
+    taskText: "Arrumar cama",
+    status: "ativo",
+  },
+];
